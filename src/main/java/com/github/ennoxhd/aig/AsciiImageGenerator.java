bc5c958b3be016aa3728dae32cc821ac6da8ee24
EnnoxHD <henrik.schirmacher@outlook.de>
2021-03-28 20:14:54 +0200

    Refactor main() method

--- src/main/java/com/github/ennoxhd/aig/AsciiImageGenerator.java
+++ src/main/java/com/github/ennoxhd/aig/AsciiImageGenerator.java
@@ -3,25 +3,22 @@ package com.github.ennoxhd.aig;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.Optional;
+import java.io.IOException;
 
 public final class AsciiImageGenerator {
 	
 	public static void main(final String[] args) {
 		try {
-			Optional<File> optImageFile = Dialogs.chooseImageFileDialog();
-			Optional<Point2D.Double> optScaling = Dialogs.chooseScalingFactorsDialog();
-			if(optImageFile.isEmpty() || optScaling.isEmpty()) System.exit(1);
-			Optional<File> optTextFile = FileUtils.getOutputFile(optImageFile.get());
-			if(optTextFile.isEmpty()) System.exit(2);
-			Optional<BufferedImage> optImage = FileUtils.getImageFromFile(optImageFile.get(), optScaling.get());
-			if(optImage.isEmpty()) System.exit(3);
-			Optional<String[]> optAsciiImage = Converter.convertToAscii(optImage.get());
-			if(optAsciiImage.isEmpty()) System.exit(4);
-			if(!FileUtils.writeToFile(optAsciiImage.get(), optTextFile.get())) System.exit(5);
+			File imageFile = Dialogs.chooseImageFileDialog().orElseThrow();
+			Point2D.Double scalingFactors = Dialogs.chooseScalingFactorsDialog().orElseThrow();
+			File textFile = FileUtils.getOutputFile(imageFile).orElseThrow();
+			BufferedImage image = FileUtils.getImageFromFile(imageFile, scalingFactors).orElseThrow();
+			String[] asciiImage = Converter.convertToAscii(image).orElseThrow();
+			if(!FileUtils.writeToFile(asciiImage, textFile))
+				throw new IOException("Could not write to file.");
 		} catch(Exception e) {
 			e.printStackTrace();
-			System.exit(6);
+			System.exit(1);
 		}
 		System.exit(0);
 	}
